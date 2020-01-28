cc938c8c2a52840aa452516a26cd5080c27d3c9a
Andy Laken <andy@coverus.io>
2020-01-27 20:11:13 -0800

    Update settings.py with lines from pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # other
 
 LOGIN_REDIRECT_URL = '/'
